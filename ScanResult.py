8d143e1c6294023aee44ee908a1ba0314329cdaf
Raihandany <raihandaniy@gmail.com>
2024-06-19 23:59:12 +0700

    Scan Result Change Schema

--- ScanResult.py
+++ ScanResult.py
@@ -4,24 +4,26 @@
 
 class ScanResult:
 
-    def __init__(self, scanner_id: str, scan_time: str, mac_addresses: list):
-        self.scanner_id = scanner_id
-        self.scan_time = scan_time
-        self.mac_addresses = mac_addresses
+    def __init__(self, id_scan: str, boardcomputer_name: str, mac_address_scan: list, ip_address_scan: str, timetaken: str):
+        self.id_scan = id_scan
+        self.boardcomputer_name = boardcomputer_name
+        self.mac_address_scan = mac_address_scan
+        self.ip_address_scan = ip_address_scan
+        self.timetaken = timetaken
 
     def to_json(self) -> str:
         return ('{'
-                '"scannerId":"' + self.scanner_id + '", '
-                '"scanTime": "' + self.scan_time + '", '
-                '"macAddresses": ' + (self.serialized_mac_addresses()) + '}')
+                '"id_scan":"' + self.id_scan + '", '
+                '"boardcomputer_name": "' + self.boardcomputer_name + '", '
+                '"ip_address_scan": "' + self.ip_address_scan + '", '
+                '"timetaken": "' + self.timetaken + '", '
+                '"mac_address_scan": ' + self.serialized_mac_address_scan() + '}')
 
-    def serialized_mac_addresses(self) -> str:
-        count = 0
+    def serialized_mac_address_scan(self) -> str:
         serialized = '['
-        for addr in self.mac_addresses:
+        for count, addr in enumerate(self.mac_address_scan):
             serialized += '"' + addr + '"'
-            count += 1
-            if count < len(self.mac_addresses):
+            if count < len(self.mac_address_scan) - 1:
                 serialized += ', '
         serialized += ']'
         return serialized
